a4b8bfcb04153366feace8f93cfeed13774d6a32
BobbyK1 <marbobkara@gmail.com>
2023-12-19 21:22:28 -0600

    Updated render engine and layout

--- src/app/(center)/(routes)/post/UI/RichTextEmbed.js
+++ src/app/(center)/(routes)/post/UI/RichTextEmbed.js
@@ -1,36 +1,65 @@
-'use client'
-
 import { BLOCKS, MARKS } from '@contentful/rich-text-types';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Heading, Link } from "@chakra-ui/react";
 
 export default function RichTextEmbed({ page }) {
     const Bold = ({ children }) => <Text fontWeight="bold">{children}</Text>;
 
     const TextBlock = ({ children }) => (
-        <Box>{children}</Box>
+        <Box mb="4">{children}</Box>
     );
 
+    const HeadingBlock = ({ level, children }) => {
+        return (
+            <Heading as={`h${level}`} fontSize={level === 1 ? "xl" : level === 2 ? "lg" : "md"} mt="6" mb="4">
+                {children}
+            </Heading>
+        );
+    };
+
     const ListItem = ({ children }) => (
-        <ol>
-            <Text as="span" fontWeight="semibold">{children}</Text>
-        </ol>
-    );
-      
-    const OrderedListItem = ({ children }) => (
-        <Text as="li" mt="10" mb="5" fontWeight="semibold">
+        <Box as="li" mt="2" mb="2" pl="4" fontWeight="semibold">
             {children}
-        </Text>
+        </Box>
+    );
+
+    const OrderedListItem = ({ children }) => (
+        <ListItem>
+            <ol>{children}</ol>
+        </ListItem>
+    );
+
+    const Blockquote = ({ children }) => (
+        <Box borderLeftWidth="thin" borderColor="gray" pl="4" fontStyle="italic" my="4">
+            {children}
+        </Box>
     );
 
     const options = {
         renderMark: {
-        [MARKS.BOLD]: (text) => <Bold>{text}</Bold>,
+            [MARKS.BOLD]: (text) => <Bold>{text}</Bold>,
         },
         renderNode: {
-        [BLOCKS.PARAGRAPH]: (node, children) => <TextBlock>{children}</TextBlock>,
-        [BLOCKS.OL_LIST]: (node, children) => <ListItem>{children}</ListItem>,
-        [BLOCKS.LIST_ITEM]: (node, children) => <OrderedListItem>{children}</OrderedListItem>
+            [BLOCKS.PARAGRAPH]: (node, children) => <TextBlock>{children}</TextBlock>,
+            [BLOCKS.HEADING_1]: (node, children) => <HeadingBlock level={1}>{children}</HeadingBlock>,
+            [BLOCKS.HEADING_2]: (node, children) => <HeadingBlock level={2}>{children}</HeadingBlock>,
+            [BLOCKS.HEADING_3]: (node, children) => <HeadingBlock level={3}>{children}</HeadingBlock>,
+            [BLOCKS.OL_LIST]: (node, children) => <Box as="ol" pl="4" mb="4">{children}</Box>,
+            [BLOCKS.UL_LIST]: (node, children) => <Box as="ul" pl="4" mb="4">{children}</Box>,
+            [BLOCKS.LIST_ITEM]: (node, children) => <ListItem>{children}</ListItem>,
+            [BLOCKS.HR]: () => <Box borderBottom="0.5px solid gray" my="4" />,
+            [BLOCKS.EMBEDDED_ASSET]: (node) => {
+                const { data } = node;
+                return (
+                    <Box>
+                        <img src={data.target.fields.file.url} alt={data.target.fields.description} />
+                    </Box>
+                );
+            },
+            [BLOCKS.HYPERLINK]: (node, children) => {
+                return <Link href={node.data.uri} target="_blank" rel="noopener noreferrer">{children}</Link>;
+            },
+            [BLOCKS.QUOTE]: (node, children) => <Blockquote>{children}</Blockquote>,
         },
         renderText: (text) => text,
     };
--- src/app/(center)/(routes)/post/[postId]/page.js
+++ src/app/(center)/(routes)/post/[postId]/page.js
@@ -1,9 +1,11 @@
 import client from "@/app/utils/contentful";
 import { Suspense } from "react";
 import YoutubeEmbed from "../UI/YoutubeEmbed";
-import { Container, Heading } from "@chakra-ui/react";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, Container, Heading } from "@chakra-ui/react";
 import GoogleSlideEmbed from "../UI/GoogleSlideEmbed";
 import RichTextEmbed from "../UI/RichTextEmbed";
+import Link from "next/link";
+import { ChevronRight } from "@/app/(center)/UI/icons";
 
 export default async function Page({ params }) {
     const id = params.postId;
@@ -16,58 +18,92 @@ export default async function Page({ params }) {
         return null;
     }
 
-    async function Post() {
+    async function GetPost() {
         const post = await client.getEntry(id)
             .then(entry => {return entry})
 
-        switch (post.fields.mediaType) {
-            case "Video":
-                return (
-                    <>
-                        <Heading fontSize={["2xl", "3xl"]} mb="10">{post.fields.title}</Heading>
-                        <YoutubeEmbed videoId={extractVideoId(post.fields.videoLink)} height="400" />
-                    </>
-                )
-                break;
+        return post;
+
+        // switch (post.fields.mediaType) {
+        //     case "Video":
+        //         return (
+        //             <>
+        //                 <Heading fontSize={["2xl", "3xl"]} mb="10">{post.fields.title}</Heading>
+        //                 <YoutubeEmbed videoId={extractVideoId(post.fields.videoLink)} height="400" />
+        //             </>
+        //         )
+        //         break;
             
-            case "PDF":
-                return <PdfEmbed link={post.fields.videoLink} />
-                break;
+        //     case "PDF":
+        //         return <PdfEmbed link={post.fields.videoLink} />
+        //         break;
 
-            case "Image":
-                return (
-                    <>
-                        <Heading mb="10">{post.fields.title}</Heading>
-                        <ImageEmbed link={post.fields.videoLink} />
-                    </>
-                )
-                break;
+        //     case "Image":
+        //         return (
+        //             <>
+        //                 <Heading mb="10">{post.fields.title}</Heading>
+        //                 <ImageEmbed link={post.fields.videoLink} />
+        //             </>
+        //         )
+        //         break;
 
-            case "Google Slide":
-                return ( 
-                    <>
-                        <Heading mb="10">{post.fields.title}</Heading>
-                        <GoogleSlideEmbed link={post.fields.videoLink} />
-                    </>
-                )
-                break;
+        //     case "Google Slide":
+        //         return ( 
+        //             <>
+        //                 <Heading mb="10">{post.fields.title}</Heading>
+        //                 <GoogleSlideEmbed link={post.fields.videoLink} />
+        //             </>
+        //         )
+        //         break;
         
-            case "Blog": 
-                return (
-                    <>
-                        <Heading mb="10">{post.fields.title}</Heading>
-                        <RichTextEmbed page={post.fields.postBody} />
-                    </>
-                )
-                break;
-            default:
-                break;
-        }
+        //     case "Blog": 
+        //         return (
+        //             <>
+        //                 <Heading mb="10">{post.fields.title}</Heading>
+        //                 <RichTextEmbed page={post.fields.postBody} />
+        //             </>
+        //         )
+        //         break;
+        //     default:
+        //         break;
+        // }
     }
+
+    const post = await GetPost();
+    const mediaType = post.fields.mediaType;
+
+    async function GetCollection() {
+        const collection = await client.getContentType(post.sys.contentType.sys.id)
+            .then(data => {
+                return { name: data.name, slug: data.sys.id};
+            })
+
+        return collection;
+    }
+
+    const collection = await GetCollection();
+
     return (
-        <Container maxW="5xl" pb="20" my="10">
+        <Container maxW="container.sm" pb="20" my="10">
+            <Breadcrumb mb="10" fontSize={["xs", "xs", "sm"]}  separator={<ChevronRight fontSize="xs" />}>
+                <BreadcrumbItem>
+                    <BreadcrumbLink fontSize={["xs", "xs", "sm"]} as={Link} href="/collections">All Collections</BreadcrumbLink>
+                </BreadcrumbItem>
+
+                <BreadcrumbItem>
+                    <BreadcrumbLink fontSize={["xs", "xs", "sm"]} as={Link} href={`/collections/${collection.slug}`}>{collection.name}</BreadcrumbLink>
+                </BreadcrumbItem>
+
+                <BreadcrumbItem>
+                    <BreadcrumbLink isCurrentPage={true} fontSize={["xs", "xs", "sm"]}>{post.fields.title}</BreadcrumbLink>
+                </BreadcrumbItem>
+            </Breadcrumb>
             <Suspense fallback="Loading...">
-                <Post />
+                <Heading mb="10">{post.fields.title}</Heading>
+
+                {mediaType === "Blog" && <RichTextEmbed page={post.fields.postBody} />}
+                {mediaType === "Video" && <YoutubeEmbed videoId={extractVideoId(post.fields.videoLink)} />}
+                {mediaType === "Google Slide" && <GoogleSlideEmbed link={post.fields.videoLink} />}
             </Suspense>
         </Container>
         
